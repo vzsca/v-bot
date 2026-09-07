7a519079fe9482bb526b94dc6db4f04bb5d561eb
vzsca <lord.vesca@gmail.com>
2026-09-07 22:31:08 +0200

    test: cover updater rollback behavior

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,12 +1,28 @@
-from updater import current_commit, requirements_changed
+import subprocess
+
+import updater
 
 
 def test_current_commit_is_a_sha_or_empty():
-    commit = current_commit()
+    commit = updater.current_commit()
     assert commit == "" or (len(commit) == 40 and all(char in "0123456789abcdef" for char in commit))
 
 
 def test_requirements_changed_returns_false_for_same_commit():
-    assert requirements_changed("abc", "abc") is False
-    assert requirements_changed("", "def") is False
-    assert requirements_changed("abc", "") is False
+    assert updater.requirements_changed("abc", "abc") is False
+    assert updater.requirements_changed("", "def") is False
+    assert updater.requirements_changed("abc", "") is False
+
+
+def test_rollback_code_uses_hard_reset(monkeypatch):
+    calls = []
+
+    def fake_run_git(*args, **kwargs):
+        calls.append(args)
+        return subprocess.CompletedProcess(args=["git", *args], returncode=0, stdout="", stderr="")
+
+    monkeypatch.setattr(updater, "_run_git", fake_run_git)
+    ok, message = updater.rollback_code("a" * 40)
+    assert ok is True
+    assert "aaaaaaaa" in message
+    assert calls == [("reset", "--hard", "a" * 40)]
