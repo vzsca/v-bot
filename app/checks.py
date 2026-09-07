7a46c02922199eb04a332454d1646b144bff542a
vzsca <lord.vesca@gmail.com>
2026-09-07 21:44:05 +0200

    fix: restore app module imports

--- app/checks.py
+++ app/checks.py
@@ -2,8 +2,9 @@
 
 from discord.ext import commands
 
-from core import config, exceptions
-from core.state import state
+import config
+import exceptions
+from state import state
 
 
 def is_permanent_owner(user_id: int) -> bool:
